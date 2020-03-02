e4aa7fd828ef8360127d293306e851adfbb96d5f
Kragel <61646979+Kragel@users.noreply.github.com>
2020-03-02 21:43:29 +0300

    Add files via upload

---
+++ ex28/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        long i1 = 0177;
+        System.out.println("Binary is  " + Long.toBinaryString(i1));
+        i1 = 0x2f;
+        System.out.println("Binary is " + Long.toBinaryString(i1));
+
+    }
+}
